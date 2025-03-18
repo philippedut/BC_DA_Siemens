56befa5269c0eb05da2c648d673ebedfc7e8b83a
Philippe Dutranoit <philoudutranoit@gmail.com>
2025-03-18 08:54:33 +0000

    adding functions to the notebook

---
+++ Notebooks/Functions.py
@@ -0,0 +1,72 @@
+import os
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import matplotlib.ticker as ticker
+import matplotlib.dates as mdates
+import seaborn as sns
+from statsmodels.tsa.seasonal import seasonal_decompose
+from scipy.stats import shapiro
+from statsmodels.tsa.stattools import adfuller
+
+def get_sales_by_product_group(df_sales, product_group):
+    sales_agg_group = df_sales[df_sales['Mapped_GCK'] == product_group].groupby('DATE')['Sales_EUR'].sum().reset_index()
+    return sales_agg_group
+
+"""
+We performed the Shapiro-Wilk test for normality on the sales data. The hypothesis are:
+
+-H0: The data is normally distributed.
+-H1: The data is not normally distributed.
+"""
+
+def shapiro_wilk_test(sales_data):
+    """
+    Perform the Shapiro-Wilk test for normality on a given product group’s sales data.
+    
+    Args:
+    sales_data (pd.Series): Time series sales data for a single product group.
+    
+    Returns:
+    dict: Contains the Shapiro-Wilk statistic and p-value.
+    """
+
+    # Run Shapiro-Wilk test
+    shapiro_results = shapiro(sales_data)
+
+    # Store results
+    result = {'Statistic': shapiro_results.statistic, 'p-value': shapiro_results.pvalue}
+
+    # Print results
+    print(f"Shapiro-Wilk Test for Normality:")
+    print(f"Statistic: {shapiro_results.statistic:.6f}")
+    print(f"p-value: {shapiro_results.pvalue:.6f}")
+
+    # Interpretation
+    if shapiro_results.pvalue > 0.05:
+        print(f"\n✅ The p-value ({shapiro_results.pvalue:.6f}) is greater than 0.05.")
+        print(f"   We fail to reject the null hypothesis, meaning the data appears normally distributed.\n")
+    else:
+        print(f"\n❌ The p-value ({shapiro_results.pvalue:.6f}) is less than 0.05.")
+        print(f"   We reject the null hypothesis, meaning the data is not normally distributed.\n")
+
+    return result
+
+def plot_distribution(sales_data, product_group):
+    """
+    Plots the histogram and KDE for a product group's sales data, to visualize the distribution of the Product Groups that are not normally distributed.
+    
+    Args:
+    sales_data (pd.Series): Sales data for a single product group.
+    product_group (int): The product group identifier.
+    """
+
+    plt.figure(figsize=(8, 5))
+
+    # Histogram & KDE
+    sns.histplot(sales_data, bins=30, kde=True, color="blue")
+    plt.title(f"Histogram & KDE - Product Group {product_group}")
+    plt.xlabel("Sales (€)")
+    plt.ylabel("Frequency")
+
+    plt.show()
